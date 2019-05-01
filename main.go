dcabd0ad9da579bf78e97b762d18ecbde250bf21
Jackson Hughes <jxhughes95@gmail.com>
2019-05-01 22:03:00 +0100

    Add basic web server

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
-// todo: webserver listening on listenPort
-// todo: gohtml templates for front end / form data
-// todo: function to write data from form data to file at uploadPath
+/*
+done: webserver listening on listenPort
+todo: gohtml templates for front end / form data
+todo: function to write data from form data to file at uploadPath
+*/
 
 import (
 	"flag"
 	"fmt"
+	"net/http"
 	"strconv"
 )
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Method:", r.Method)
+
+	if r.Method == "GET" {
+
+	}
+}
 
 func main() {
 
@@ -20,5 +31,20 @@ func main() {
 	uploadPathInt := *uploadPath
 
 	fmt.Println("The web server will listen on port:" + portString + " and upload files to " + uploadPathInt)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is a website server by a Go HTTP server.")
+	})
+
+	// Super simple Health check - returns 200 OK if up
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "pong")
+	})
+
+	// Upload function
+
+	http.HandleFunc("/upload", upload)
+
+	http.ListenAndServe(":"+portString, nil)
 
 }
